76def54be0140891d2e123782470e204dc1fd7d2
LanselonX <101521725+LanselonX@users.noreply.github.com>
2023-01-22 22:37:19 +0200

    fix createTeam(logic about token)

--- middlewaree/createTeam.js
+++ middlewaree/createTeam.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const authMiddleware = require("./authMiddleware");
+const { secret } = require("../config");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.post("createTeam", authMiddleware, (req, res) => {
-  const data = req.body;
-  const teamName = data.name;
-  const teamMembers = data.members;
-  const teamCountry = data.country;
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(400).json({ message: "Token validation error" });
+  }
+  try {
+    const decoded = jwt.verify(token, secret);
+    const data = req.body;
+    const teamName = data.name;
+    const teamMembers = data.members;
+    const teamCountry = data.country;
 
-  console.log(`Team Name: ${teamName}`);
-  console.log(`Team Members: `);
-  teamMembers.forEach((member) => {
-    console.log(member);
-  });
-  console.log(`Team Country: ${teamCountry}`);
+    console.log(`Team Name: ${teamName}`);
+    console.log(`Team Members: `);
+    teamMembers.forEach((member) => {
+      console.log(member);
+    });
+    console.log(`Team country: ${teamCountry}`);
 
-  //Псевдо логика для тимы, только надо выцеплять данные из req.body, пока что хз как их брать для проверки
-
-  res.send({ message: "Team created " });
+    res.send({ message: "Team created " });
+  } catch (e) {
+    return res.status(400).json({ message: "invalid token" });
+  }
 });
